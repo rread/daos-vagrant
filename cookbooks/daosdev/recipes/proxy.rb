ff5d7f7c9fbf59124139f553c6c6a0401cdff8e7
Robert Read <robert.read@intel.com>
2017-01-10 12:13:44 -0800

    Include proxy.rb

---
+++ cookbooks/daosdev/recipes/proxy.rb
@@ -0,0 +1,35 @@
+proxy = nil
+
+if node['proxy']['http_proxy']
+    ENV['http_proxy'] = node['proxy']['http_proxy']
+    proxy = node['proxy']['http_proxy']
+end
+
+if node['proxy']['https_proxy']
+    ENV['https_proxy'] = node['proxy']['https_proxy']
+    proxy = node['proxy']['https_proxy']
+end
+
+
+if proxy
+    node.default['yum']['main']['proxy'] = proxy
+
+    file '/etc/profile.d/proxy.sh' do
+        content <<-EOH
+export http_proxy=#{node['proxy']['http_proxy']}
+export https_proxy=#{node['proxy']['https_proxy']}
+export HTTP_PROXY=#{node['proxy']['http_proxy']}
+export HTTPS_PROXY=#{node['proxy']['https_proxy']}
+export no_proxy=#{node['proxy']['no_proxy']}
+EOH
+        owner "root"
+        group "root"
+        mode 0644
+        notifies :restart, 'service[network]', :delayed
+    end
+else
+    file '/etc/profile.d/proxy.sh' do
+        action :delete
+        notifies :restart, 'service[network]', :delayed
+    end
+end
